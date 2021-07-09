23eed6ef0e1fdf2ba793d57e484aa29f06cc147a
Mikhail Siomin <victorovich.01@mail.ru>
2021-07-09 10:05:01 +0300

    edit

---
+++ src/ROOT/WEB-INF/classes/NewtonPolynom.java
@@ -0,0 +1,32 @@
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+@WebServlet("/polynom")
+public class NewtonPolynom extends HttpServlet
+{  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException 
+    {      
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+ 
+        String nodes = request.getParameter("Nodes");
+        String method = request.getParameterValues("Method");
+         
+        try
+	{
+            writer.println("<p>Nodes: " + nodes + "</p>");
+            writer.println("<p>Method: " + method + "</p>");
+        }
+	finally
+	{
+            writer.close();  
+        }
+    }
+
+}
