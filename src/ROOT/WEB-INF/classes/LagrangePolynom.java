d60f7dd49047cd0806d67d9b47a92b10cbe73a99
Mikhail Siomin <victorovich.01@mail.ru>
2021-07-10 04:20:21 +0300

    edit

--- src/ROOT/WEB-INF/classes/LagrangePolynom.java
+++ src/ROOT/WEB-INF/classes/LagrangePolynom.java
@@ -23,7 +23,7 @@ public class LagrangePolynom extends HttpServlet
         String step = request.getParameter("Step");
         String[] method = request.getParameterValues("Method");
 
-        String[] commands = { "/usr/local/tomcat/webapps/calc_polynom/polynom.bin" };
+        String[] commands = { "bash", "-c", "/usr/local/tomcat/webapps/calc_polynom/polynom.bin" };
  
         try
 	{
