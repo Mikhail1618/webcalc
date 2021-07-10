091e159220e707e2dd7a70049e6e80b7e937ce6d
Mikhail Siomin <victorovich.01@mail.ru>
2021-07-10 05:58:05 +0300

    edit

--- src/calc_polynom/main.cpp
+++ src/calc_polynom/main.cpp
@@ -11,8 +11,8 @@ void fun(char * str)
     std::string midle = ";";
     std::string close = ")";
 
-    std::cout << s.substr(s.find(open)+1, s.find(midle)) << "\n";
-    std::cout << s.substr(s.find(midle), s.find(close)-1) << "\n";
+    std::cout << s.substr(s.find(open)+1, s.find(midle)-1) << "\n";
+    std::cout << s.substr(s.find(midle)+1, s.find(close)-1) << "\n";
 }
 
 
