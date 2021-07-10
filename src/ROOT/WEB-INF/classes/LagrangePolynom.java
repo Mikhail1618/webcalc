c7368764de499918ab31507130a150bab90aba1a
Mikhail Siomin <victorovich.01@mail.ru>
2021-07-10 03:02:13 +0300

    edit

--- src/ROOT/WEB-INF/classes/LagrangePolynom.java
+++ src/ROOT/WEB-INF/classes/LagrangePolynom.java
@@ -49,7 +49,7 @@ public class LagrangePolynom extends HttpServlet
        {
             writer.println(s);
        }
-       writer.println(</p>);
+       writer.println("</p>");
        writer.close();
 
     }
