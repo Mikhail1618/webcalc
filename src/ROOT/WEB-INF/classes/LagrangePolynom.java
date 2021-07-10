7c6574d34756de373efe6dca8461618e61ba8712
Mikhail Siomin <victorovich.01@mail.ru>
2021-07-10 05:14:07 +0300

    edit

--- src/ROOT/WEB-INF/classes/LagrangePolynom.java
+++ src/ROOT/WEB-INF/classes/LagrangePolynom.java
@@ -23,7 +23,7 @@ public class LagrangePolynom extends HttpServlet
         String step = request.getParameter("Step");
         String[] method = request.getParameterValues("Method");
 
-        String[] commands = { "polynom.bin", nodes};
+        String[] commands = { "polynom.bin", nodes, start, end};
  
         try
 	{
